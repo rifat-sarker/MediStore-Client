b1c5aef5a1ae9f0ba040151feb3a2f942159d93f
rifat-sarker <rifatswd@gmail.com>
2025-03-08 23:08:03 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import "./globals.css";
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center">Home page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // if you're using the src/ folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

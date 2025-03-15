552c609087cd9eded80352c33a922da51183c4cb
rifat-sarker <rifatswd@gmail.com>
2025-03-15 23:23:17 +0600

    add wishlist functionality to ProductCard; implement addToWishlist service

--- src/components/modules/Products/ProductCard.tsx
+++ src/components/modules/Products/ProductCard.tsx
@@ -11,28 +11,40 @@ import {
 
 import { IProduct } from "@/types/product";
 
-import { Heart, PlusIcon, ShoppingCart, Star } from "lucide-react";
+import {
+  Heart,
+  PlusIcon,
+  ShoppingCart,
+  Star,
+  WashingMachine,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const ProductCard = ({ product }: { product: IProduct }) => {
+  const handleAddToWishlit = () => {
+    console.log("Add to wishlist");
+  };
   return (
-    <Link href={`/medicine/${product._id}`}>
-      <div className="border p-4 rounded-md h-[280px] flex flex-col justify-between items-center text-center">
+    <div className="relative border p-4 rounded-md h-[280px] flex flex-col justify-between items-center text-center">
+      <Link href={`/medicine/${product._id}`}>
         <img
           src={product.image}
           alt={product.name}
           className="w-20 h-20 object-cover rounded-md"
         />
-        <div className="flex-1 flex flex-col justify-center">
-          <h2 className="text-sm font-medium">{product.name}</h2>
-          <p className="text-gray-600 text-sm mt-1">MRP. {product.price}</p>
-        </div>
-        <button className="mt-auto text-sm font-semibold border-primary text-primary border px-4 py-2 w-full flex items-center justify-center gap-1 hover:text-white hover:bg-primary">
-          <PlusIcon className="size-4" /> Add to Cart
-        </button>
+      </Link>
+      <button onClick={handleAddToWishlit} className="absolute right-2 top-2">
+        <Heart className="size-6 text-primary" />
+      </button>
+      <div className="flex-1 flex flex-col justify-center">
+        <h2 className="text-sm font-medium">{product.name}</h2>
+        <p className="text-gray-600 text-sm mt-1">MRP. {product.price}</p>
       </div>
-    </Link>
+      <button className="mt-auto text-sm font-semibold border-primary text-primary border px-4 py-2 w-full flex items-center justify-center gap-1 hover:text-white hover:bg-primary">
+        <PlusIcon className="size-4" /> Add to Cart
+      </button>
+    </div>
   );
 };
 
---
+++ src/services/Wishlist/index.ts
@@ -0,0 +1,23 @@
+"user server"
+
+export const addToWishlist = async (productId: string) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_API}/wishlist`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ productId }),
+        next: {
+          tags: ["Wishlist"],
+        },
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (error: any) {
+    return Error(error.message);
+  }
+}

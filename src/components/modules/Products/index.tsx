abf71d75cbaae39b709c6bc4fd68e461a9c071f4
rifat-sarker <rifatswd@gmail.com>
2025-07-15 20:12:17 +0600

    refactor AllProducts component layout; remove console log and improve grid structure

--- src/components/modules/Products/index.tsx
+++ src/components/modules/Products/index.tsx
@@ -18,7 +18,6 @@ const AllProducts = ({
   const [filteredProducts, setFilteredProducts] =
     useState<IProduct[]>(products);
 
-    console.log(products);
   useEffect(() => {
     const selectedCategory = searchParams.get("category");
     const maxPrice = searchParams.get("maxPrice");
@@ -30,7 +29,7 @@ const AllProducts = ({
         const categoryName =
           typeof product.category === "string"
             ? product.category
-            : (product.category as ICategory)?.name; // If category is an object, get its name
+            : (product.category as ICategory)?.name;
 
         return categoryName?.toLowerCase() === selectedCategory.toLowerCase();
       });
@@ -46,12 +45,17 @@ const AllProducts = ({
   }, [searchParams, products]);
 
   return (
-    <div className="flex gap-8 my-10">
-      <FilterSidebar categories={categories} />
-      <div>
+    <div className="flex flex-col lg:flex-row gap-8 my-10">
+      {/* Sidebar */}
+      <div className="w-full lg:w-1/4">
+        <FilterSidebar categories={categories} />
+      </div>
+
+      {/* Products Grid */}
+      <div className="w-full lg:w-3/4">
         <h2 className="text-xl font-bold my-4">All Products</h2>
         {filteredProducts?.length > 0 ? (
-          <div className="grid grid-cols-6 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
             {filteredProducts.map((product, idx) => (
               <ProductCard key={idx} product={product} />
             ))}
